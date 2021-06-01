58db028c5abd34bb5d49d1ffa2ea5281e18ed04e
juyingguo <juying2050@sina.cn>
2021-06-01 15:17:36 +0800

    1.add avformat_find_stream_info for get video head info 2.AVStream info print

--- ndk-android-project/testffmpeg/app/src/main/cpp/native-lib.cpp
+++ ndk-android-project/testffmpeg/app/src/main/cpp/native-lib.cpp
@@ -22,6 +22,7 @@
 **
 *******************************************************************************/
 //！！！！！！！！！ 加群23304930下载代码和交流
+
 #include <jni.h>
 #include <string>
 #include <android/log.h>
@@ -30,6 +31,17 @@
 extern "C"{
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+}
+#include<iostream>
+using namespace std;
+
+static double r2d(AVRational r)
+{
+    LOGW("r2d,r.num = %d,r.den=%d ",
+         r.num,
+         r.den
+    );
+    return r.num==0||r.den == 0 ? 0 :(double)r.num/(double)r.den;
 }
 
 extern "C"
@@ -52,18 +64,62 @@ Java_aplay_testffmpeg_MainActivity_stringFromJNI(
     av_register_all();
     //初始化网络
     avformat_network_init();
+
     //打开文件
     AVFormatContext *ic = NULL;
-    char path[] = "/sdcard/paiDuiGe.mp4";
+//    char path[] = "/sdcard/paiDuiGe.mp4";
+    char path[] = "/sdcard/paiDuiGe.flv";
     int re = avformat_open_input(&ic,path,0,0);
-    if(re == 0)
-    {
-       LOGW("avformat_open_input %s success!",path);
-    }
-    else
+    if(re != 0)
     {
         LOGW("avformat_open_input failed!:%s",av_err2str(re));
+        return env->NewStringUTF(hello.c_str());
     }
+    LOGW("avformat_open_input %s success!",path);
+    //获取流信息
+    re = avformat_find_stream_info(ic,0);
+    if(re != 0)
+    {
+        LOGW("avformat_find_stream_info failed!");
+    }
+    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams);
+
+    int fps = 0;
+    int videoStream = 0;
+    int audioStream = 1;
+
+    for(int i = 0; i < ic->nb_streams; i++)
+    {
+        AVStream *as = ic->streams[i];
+        if(as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            LOGW("视频数据");
+            videoStream = i;
+            fps = static_cast<int>(r2d(as->avg_frame_rate));
+
+            LOGW("fps = %d,width=%d height=%d codeid=%d pixformat=%d",fps,
+                 as->codecpar->width,
+                 as->codecpar->height,
+                 as->codecpar->codec_id,
+                 as->codecpar->format
+            );
+        }
+        else if(as->codecpar->codec_type ==AVMEDIA_TYPE_AUDIO )
+        {
+            LOGW("音频数据");
+            audioStream = i;
+            LOGW("sample_rate=%d channels=%d sample_format=%d",
+                 as->codecpar->sample_rate,
+                 as->codecpar->channels,
+                 as->codecpar->format
+            );
+        }
+    }
+    //ic->streams[videoStream];
+
+
+    //关闭上下文
+    avformat_close_input(&ic);
     return env->NewStringUTF(hello.c_str());
 }
 extern "C"
