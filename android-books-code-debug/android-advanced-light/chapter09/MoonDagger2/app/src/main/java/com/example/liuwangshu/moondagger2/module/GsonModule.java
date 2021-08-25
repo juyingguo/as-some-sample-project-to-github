66296dace226c5ca7b541aa0668dde90bd3997cf
juyingguo <juying2050@sina.cn>
2021-08-25 17:07:53 +0800

    android-advanced-light:refactor dir

---
+++ android-books-code-debug/android-advanced-light/chapter09/MoonDagger2/app/src/main/java/com/example/liuwangshu/moondagger2/module/GsonModule.java
@@ -0,0 +1,24 @@
+package com.example.liuwangshu.moondagger2.module;
+
+import com.example.liuwangshu.moondagger2.annotation.ApplicationScope;
+import com.google.gson.Gson;
+
+import javax.inject.Scope;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Administrator on 2016/12/18 0018.
+ */
+
+@Module
+public class GsonModule {
+//    @ApplicationScope
+//    @Singleton
+    @Provides
+    public Gson provideGson() {
+        return new Gson();
+    }
+}
